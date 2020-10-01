320204fb4159d98419001b15fabe56c066270928
Snorre Sulheim <ssulheim@gmail.com>
2020-10-01 11:32:16 +0000

    Bug fix

--- Scripts/strain_design.py
+++ Scripts/strain_design.py
@@ -49,7 +49,7 @@ def BGC_optknock(model, pathway_fn, optknock_reactions = [], fraction_of_optimum
         results.set_index("ID", inplace = True)
         
         relative_results = results/results.loc["WT", :]
-        improved_results = relative_results.loc[relative_results["Production"]>(1+EPSION_FRACTION), :]
+        improved_results = relative_results.loc[relative_results["Production"]>(1+EPSILON_FRACTION), :]
 
         # improved_results = results.loc[results["Production"] > results.loc["WT", "Production"] + EPSILON, :]
         # improved_results = improved_results/results.loc["WT", :]
